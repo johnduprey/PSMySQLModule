d1ca8f497db674106d4bec608e8ebda396624936
guitarrapc <cherishacquire@outlook.com>
2014-01-10 17:42:45 +0900

    added partition check

--- PSMySQLModule/config/PSMySQLModule-config.ps1
+++ PSMySQLModule/config/PSMySQLModule-config.ps1
@@ -22,10 +22,24 @@ $PSMySQLModule.GetScheme = New-Object psobject -property @{
 
 # contains MySQL Get Column Query
 $PSMySQLModule.GetColumn = New-Object psobject -property @{
-    query                  = "SELECT column_name AS COLUMNNAME, data_type AS DATATYPE, is_nullable AS IsNullable, column_default AS COLUMNDEFAULT, table_name AS TABLENAME, Table_schema AS SCHEMANAME, COLUMN_COMMENT FROM INFORMATION_SCHEMA.COLUMNS"
+    query                  = "SELECT column_name AS COLUMNNAME, data_type AS DATATYPE, is_nullable AS IsNullable, column_default AS COLUMNDEFAULT, table_name AS TABLENAME, Table_schema AS SCHEMANAME, COLUMN_COMMENT FROM INFORMATION_SCHEMA.COLUMNS;"
 }
 
 # contains MySQL Get Function Query
 $PSMySQLModule.GetFunction = New-Object psobject -property @{
-    query                  = "show function status;"
+    query                  = "SHOW FUNCTION STATUS;"
+}
+
+# contains MySQL Get Partition Query
+$PSMySQLModule.GetPartition = New-Object psobject -property @{
+    TableList              = @{
+        query                  = "SELECT DISTINCT(TABLE_NAME) FROM information_schema.`PARTITIONS` WHERE NODEGROUP = 'default';"
+    }
+    PartitionStatus        = @{
+        query                  = "SELECT * FROM information_schema.`PARTITIONS` WHERE TABLE_NAME = {0};"
+    }
+}
+
+$PSMySQLModule.GetTrigger = New-Object psobject -property @{
+    query                  = "SHOW TRIGGERS;"
 }
